44c7999859104ced33afd3a4812375a54ff862bf
Gasanov Nazim <nazimushka@gmail.com>
2022-12-07 23:42:27 +0300

    Результат домашней работы 4-го семинара

--- 31Task.py
+++ 31Task.py
@@ -3,7 +3,28 @@
 #     *Пример:*
 #     - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 #     - k=5 => 2*x^5 + 4*x^4 + 2*x^3 + 2*x^2 + 4*x + 5 = 0
-# *Доп задание: значения коэффициентов от -100 до 100
+# *Доп задание: значения коэффициентов от -100 до 100.
 
 
 import random
+
+k = int(input('Введите число k = '))
+coef = [random.randint(-100, 100) for i in range(0, k+1)]
+
+
+def Polynom(k, koef):
+    pol = ''
+    for i in range(0, k+1):
+        if k - i > 1 and coef[i] != 0:
+            pol += f'{coef[i]}*x^{k-1} + '
+        elif k-1 == 1 and coef[i] != 0:
+            pol += f'{coef[i]}*x + '
+        elif coef[i] == 0:
+            pol += ''
+        else:
+            pol += f'{coef[i]} = 0'
+    return pol
+
+
+with open('31file.txt', 'w') as data:
+    data.write(Polynom(k, coef).replace('+ -', '- '))
--- 32Task.py
+++ 32Task.py
@@ -1,4 +1,77 @@
-# Даны два файла, в каждом из которых находится запись многочлена. 
+# Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.(одинаковый размер уравнений)*
-# *Доп задание: для разного размера уравнения
+# *Доп задание: для разного размера уравнения.
 
+str_num_1 = ""
+str_num_2 = ""
+
+with open('32file_1_r.txt', 'r') as file:
+    for line in file:
+        str_num_1 = line
+
+with open('32file_2_r.txt', 'r') as file:
+    for line in file:
+        str_num_2 = line
+
+dict_1 = {}
+dict_2 = {}
+str_num_1 = str_num_1.split(" = ")[0]
+str_num_2 = str_num_2.split(" = ")[0]
+str_num_1_list = str_num_1.split(" + ")
+str_num_2_list = str_num_2.split(" + ")
+# print(str_num_1_list)
+# print(str_num_2_list)
+for i in range(len(str_num_1_list)):
+    if str_num_1_list[i].count("x^"):
+        dict_1[int(str_num_1_list[i].split("^")[1])] = int(
+            str_num_1_list[i].split("*")[0])
+    elif str_num_1_list[i].count("x"):
+        dict_1[1] = int(str_num_1_list[i].split("*")[0])
+    else:
+        dict_1[0] = int(str_num_1_list[i])
+print(dict_1)
+
+for i in range(len(str_num_2_list)):
+    if str_num_2_list[i].count("x^"):
+        dict_2[int(str_num_2_list[i].split("^")[1])] = int(
+            str_num_2_list[i].split("*")[0])
+    elif str_num_2_list[i].count("x"):
+        dict_2[1] = int(str_num_2_list[i].split("*")[0])
+    else:
+        dict_2[0] = int(str_num_2_list[i])
+print(dict_2)
+
+max_index = 0
+for key in dict_1.keys():
+    if max_index < key:
+        max_index = key
+for key in dict_2.keys():
+    if max_index < key:
+        max_index = key
+# print(max_index)
+
+dict_3 = {}
+for i in range(max_index, -1, - 1):
+    if i in dict_1:
+        if i in dict_2:
+            dict_3[i] = dict_1[i] + dict_2[i]
+        else:
+            dict_3[i] = dict_1[i]
+    elif i in dict_2:
+        dict_3[i] = dict_2[i]
+print(dict_3)
+
+final_list = []
+for i in range(max_index, -1, - 1):
+    # print(i, end=" ")
+    if i in dict_3:
+        # print(dict_3[i])
+        if i == 1:
+            final_list.append(str(dict_3[i]) + "*x")
+        elif i == 0:
+            final_list.append(str(dict_3[i]))
+        else:
+            final_list.append(str(dict_3[i]) + "*x^" + str(i))
+
+with open('32file_a.txt', 'a') as file:
+    print(*final_list, file=file, sep=' + ', end=' = 0\n')
